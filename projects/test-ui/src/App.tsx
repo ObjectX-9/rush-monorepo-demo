6fd161f1de5e137d25c5a4ae19a846a9c448b04c
zhuling822 <ling.zhu@js.design>
2024-08-29 19:28:42 +0800

    feat: storybook

--- projects/test-ui/src/App.tsx
+++ projects/test-ui/src/App.tsx
@@ -2,11 +2,11 @@ import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import {main} from 'test-core';
+import { main } from 'test-core';
 function App() {
   const [count, setCount] = useState(0)
 
-  useEffect(()=> {
+  useEffect(() => {
     main()
   }, [])
 
